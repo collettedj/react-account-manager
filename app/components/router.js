2e7d02aa5d7caceecb494d7ada43224344ee5e2f
David Collette <collettedj@gmail.com>
2016-04-07 19:13:55 -0700

    check that the user is authenticated before allowing them to go to the user profile page

--- app/components/router.js
+++ app/components/router.js
@@ -13,25 +13,49 @@ import AccountManagerRegister from './account-manager/register';
 import AccountManagerUserProfile from './account-manager/user-profile';
 import AccountManagerUserProfileIndex from './account-manager/user-profile-index';
 import AccountManagerClient from './account-manager/client';
+import {connect} from 'react-redux';
 
-const AppRouter = () => (
-    <Router history={hashHistory}>
-        <Route path="/" component={Home}>
-            <IndexRoute component={HomeIndex}/>
-            <Route path="/todo" component={TodoApp}/>
-            <Route path="/subreddit" component={Subreddit}/>
-        </Route>
-        <Route path="/account-manager" component={AccountManagerApp}>
-            <IndexRoute component={AccountManagerHome}/>
-            <Route path="/account-manager/login" component={AccountManagerLogin}/>
-            <Route path="/account-manager/register" component={AccountManagerRegister}/>
-            <Route path="/account-manager/user-profile" component={AccountManagerUserProfile}>
-                <IndexRoute component={AccountManagerUserProfileIndex}/>
-                <Route path="/account-manager/user-profile/client/:id" component={AccountManagerClient}/>
+let AppRouter = ({
+    isAuthenticated
+}) => { 
+    const requireAuth = (nextState, replace) => {
+        if(!isAuthenticated){
+            replace({
+                pathname:'/account-manager/login',
+                state: { nextPathname: nextState.location.pathname}
+            });
+        }
+    };
+
+    return (
+        <Router history={hashHistory}>
+            <Route path="/" component={Home}>
+                <IndexRoute component={HomeIndex}/>
+                <Route path="/todo" component={TodoApp}/>
+                <Route path="/subreddit" component={Subreddit}/>
             </Route>
-        </Route>
-    </Router>
-);
+            <Route path="/account-manager" component={AccountManagerApp}>
+                <IndexRoute component={AccountManagerHome}/>
+                <Route path="/account-manager/login" component={AccountManagerLogin}/>
+                <Route path="/account-manager/register" component={AccountManagerRegister}/>
+                <Route path="/account-manager/user-profile" component={AccountManagerUserProfile} onEnter={requireAuth}>
+                    <IndexRoute component={AccountManagerUserProfileIndex}/>
+                    <Route path="/account-manager/user-profile/client/:id" component={AccountManagerClient}/>
+                </Route>
+            </Route>
+        </Router>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.accountManager.login.isAuthenticated,
+    };
+};
+
+AppRouter = connect(
+    mapStateToProps
+)(AppRouter);
 
 export default AppRouter;
 
